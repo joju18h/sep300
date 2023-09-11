3cbc60be151ef3afc12bf24b511da9a75920f3f7
joju18h <joju18h@gmail.com>
2023-09-11 09:23:43 -0400

    week1

---
+++ week1/test.py
@@ -0,0 +1,23 @@
+import sys as s
+print("hello world")
+
+x = 5
+y = 5
+print(type(x))
+print(id(x))
+
+#dynamically typed
+x  = "hello"
+print(type(x))
+print(id(x))
+print(s.getrefcount(5))
+
+def add(x,y):
+    return x+y
+
+print(add(5,6))
+print(100//3)
+print(100/3)
+
+
+
