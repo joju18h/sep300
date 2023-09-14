d63b20ff9ff3f838f9fa3312e3e90bae2181f7c5
joju18h <joju18h@gmail.com>
2023-09-14 14:57:24 -0400

    done using requests module

---
+++ week2/lab1req.py
@@ -0,0 +1,24 @@
+import asyncio
+import requests
+
+async def fetch_data(url):
+    response = requests.get(url)
+    if(response.status_code == 200): #200 means
+        return f"{url} has returned an OK response"
+    else:
+        return f"Request failed for {url}"
+    
+async def main():
+    tasks = [
+        fetch_data("https://api.apis.guru/v2/list.json"),
+        fetch_data("https://api.publicapis.org/entries"),
+        fetch_data("https://api.artic.edu/api/v1/artworks/search?q=cats")
+    ]
+
+    results =  await asyncio.gather(*tasks)
+    print("All tasks have been executed")
+
+    for result in results:
+        print(result)
+
+asyncio.run(main())
